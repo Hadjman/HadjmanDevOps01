748ee72b7235ef639d8904d5866449aa81b93039
E. Barry <hajdman@gmail.com>
2017-03-05 12:43:09 -0500

    IIS Setup

---
+++ Microsoft/Scripts/IISConfig_v10.ps1
@@ -0,0 +1,86 @@
+﻿<#*****************************************************************************************************
+*    Script to Configure IIS websites :: version 1.0
+*    Developed By Kalith Guruge of Sri Lanka on 19/2/2017
+*
+*    This script only handle the given set of requirments and will not handle anything beyond its scope.
+
+*    Running the script is at your own risk.Developer holds no responsibility for any kind of data loss.
+*
+*******************************************************************************************************#>
+Write-Host "Installing web erver [IIS] features...."
+Install-WindowsFeature -Name web-server -IncludeManagementTools -IncludeAllSubFeature -Verbose
+
+Write-Host "Removing Directory Browsing...."
+Remove-WindowsFeature -Name Web-Dir-Browsing -Verbose
+
+Write-Host "Installing .NET framework 4.5 features...."
+Install-WindowsFeature -Name NET-Framework-45-Features -IncludeAllSubFeature -IncludeManagementTools -Verbose
+
+Write-Host "Removing Default Web Site...."
+Get-Website -Name "Default Web Site" |Remove-Website -Verbose
+
+$Cfriendlyname="consultmanager"
+
+Write-Host "`nCreating a self signed certificate for $Cfriendlyname and binding it to for all IPs for port 443."
+$Cert=New-SelfSignedCertificate -DnsName $Cfriendlyname -CertStoreLocation cert:Localmachine\My
+Set-Location IIS:\SslBindings
+$Cert|New-Item 0.0.0.0!443
+
+
+$Ppath="D:\SOC\wwwroot"#original
+
+
+$Hostpath=".qa.socccc.com"
+$port=443
+
+$apppools="cartapi","mobile","orderintake","survey","surveyapi"
+
+Write-Host "Creating App pools and Web sites..."
+foreach($pool in $apppools)
+{
+    New-WebAppPool -Name $pool
+    
+    if($pool -eq "cartapi") { $hostheader= "cart-api"+$Hostpath}
+    elseif($pool -eq "surveyapi") { $hostheader= "survey-api"+$Hostpath} 
+    elseif($pool -eq "orderintake") { $hostheader= "intake"+$Hostpath} 
+    else {$hostheader = $pool+$Hostpath}
+
+    
+    if(!(Test-Path -Path ($Ppath+$pool)))
+    {
+        New-Item -ItemType Directory -Path $Ppath$pool
+        }
+
+        New-Website -Name $pool -Port $port -PhysicalPath "$Ppath$pool" -ApplicationPool $pool -HostHeader $hostheader -Ssl
+
+
+    }
+
+
+Write-Host "Creating applications...."
+
+$devicepath=$Ppath+'mobile\wwwrootdevicemobwebapi'
+$physicianpath=$Ppath+'mobile\wwwrootphymob.webapi'
+$odrintkapipath=$Ppath+"orderintake\api"
+
+
+if(!(Test-Path -Path $devicepath))
+    {
+        New-Item -ItemType Directory -Path $devicepath
+        }
+
+if(!(Test-Path -Path $physicianpath ))
+    {
+        New-Item -ItemType Directory -Path $physicianpath
+        }
+
+if(!(Test-Path -Path $odrintkapipath))
+    {
+        New-Item -ItemType Directory -Path $odrintkapipath
+        }
+
+New-WebApplication -Site "mobile" -Name "Device" -PhysicalPath $devicepath -ApplicationPool "mobile"
+
+New-WebApplication -Site "mobile" -Name "physician" -PhysicalPath $physicianpath -ApplicationPool "mobile"
+
+New-WebApplication -Site "orderintake" -Name "orderintakeapi" -PhysicalPath $odrintkapipath -ApplicationPool "orderintake"
---
+++ Microsoft/Scripts/ramatou1_iis.ps1
@@ -0,0 +1,69 @@
+﻿#Install .Net 4.5
+#Server 2016 includes .Net 4.6/4.5.x
+
+#Install IIS Role
+Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+
+#Generate Certificate
+$DNSName = "consultmanager" #Can add FQDN if needed
+$SSLCert = New-SelfSignedCertificate -CertStoreLocation Cert:\LocalMachine\my -DnsName $DNSName
+
+#Remove Default Site
+Remove-Website -Name "Default Web Site"
+
+#Create Application Pools
+$AppPools = @(
+    ('cartapi'),
+    ('mobile'),
+    ('orderintake'),
+    ('survey'),
+    ('surveyapi')
+)
+
+foreach ($AppPool in $AppPools) {
+    New-WebAppPool -Name $AppPool
+}
+
+#Create Web Sites
+$WebSites = @(
+    ('cartapi', 'cartapi', 'D:\SOC\wwwrootcartapi', 'cart-api.qa.socccc.com'),
+    ('mobile', 'mobile', 'D:\SOC\wwwrootmobile', 'mobile.qa.socccc.com'),
+    ('orderintake', 'orderintake', 'D:\SOC\wwwrootorderintake', 'intake.qa.socccc.com'),
+    ('survey', 'survey', 'D:\SOC\wwwrootsurvey', 'survey.qa.socccc.com'),
+    ('surveyapi', 'surveyapi', 'D:\SOC\wwwrootsurveyapi', 'survey-api.qa.socccc.com')
+)
+
+foreach ($WebSite in $WebSites) {
+    $SiteName = $WebSite[0]
+    $App = $WebSite[1]
+    $Path = $WebSite[2]
+    $HostHeader = $WebSite[3]
+    New-Item -ItemType Directory -Force -Path $Path
+    New-WebSite -Name $SiteName -physicalPath $Path -ApplicationPool $App
+    New-WebBinding -Name $SiteName -Protocol https -IPAddress '*' -HostHeader $HostHeader -Port 443
+    Get-WebBinding -Port 80 -Name $SiteName | Remove-WebBinding
+    $binding = Get-WebBinding -Name $SiteName -Protocol "https"
+    $binding.AddSslCertificate($SSLCert.GetCertHashString(), "my")
+    Start-WebSite $SiteName
+    #---- uncomment next three lines for testing ----
+    #Copy-Item -Path "C:\inetpub\wwwroot\iisstart.htm" -Destination $Path\iisstart.htm
+    #Copy-Item -Path "C:\inetpub\wwwroot\iisstart.png" -Destination $Path\iisstart.png
+    #Add-Content C:\Windows\System32\drivers\etc\hosts "`n 127.0.0.1 $HostHeader"
+}
+
+
+#Create applications
+$WebApps = @(
+    ('device', 'mobile', 'D:\SOC\wwwrootmobile\wwwrootdevicemobwebapi', 'mobile'),
+    ('physician', 'mobile', 'D:\SOC\wwwrootmobile\wwwrootphymob.webapi', 'mobile'),
+    ('orderintakeapi', 'orderintake', 'D:\SOC\wwwrootorderintake\api', 'orderintake')
+)
+
+foreach ($WebApp in $WebApps) {
+    $AppName = $WebApp[0]
+    $SiteName = $WebApp[1]
+    $Path = $WebApp[2]
+    $AppPool = $WebApp[3]
+    New-Item -ItemType Directory -Force -Path $Path
+    New-WebApplication -Site $SiteName -Name $AppName -physicalPath $Path -ApplicationPool $AppPool
+}
